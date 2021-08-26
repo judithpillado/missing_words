1c065792be42d04cd39729c7845f341c92f40611
Judith Pillado <judith.pillado@gmail.com>
2021-08-26 18:18:08 -0500

    Finish solution

--- lib/absent_words.rb
+++ lib/absent_words.rb
@@ -1,3 +1,13 @@
 class AbsentWords
-
+  def missing_words(s, t)
+    if s.eql? t
+      "There are no missing words, yay!"
+    else 
+      first = s.split
+      second = t.split
+      lost_words = []
+      lost_words << first - second 
+    end
+    lost_words.flatten
+  end 
 end
