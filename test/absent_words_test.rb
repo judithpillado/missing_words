9d504d9e49458b39663e22dbb26115f690449267
Judith Pillado <judith.pillado@gmail.com>
2021-08-26 17:25:03 -0500

    Create test for missing words method

--- test/absent_words_test.rb
+++ test/absent_words_test.rb
@@ -10,5 +10,8 @@ class AbsentWordsTest < Minitest::Test
   def test_it_exists
     assert_instance_of AbsentWords, @absent_words
   end
-  
+
+  def test_it_returns_missing_words
+    assert_equal ["I", "using", "programming"], @absent_words.missing_words("I am using HackerRank to improve programming", "am HackerRank to improve")
+  end
 end 
